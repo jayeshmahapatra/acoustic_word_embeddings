e9b7bad2be0b005cc2397233be6173fb20c19cf2
Jayesh Mahapatra <jayeshmahapatra@gmail.com>
2020-08-20 05:42:39 +0200

    Corrected phoneme ser calculations

--- add_ser.py
+++ add_ser.py
@@ -4,16 +4,21 @@ import numpy as np
 
 
 
-def add_ser(wordpairs_filepath = '/data/users/jmahapatra/data/wordpairs.txt', wordpairs_ser_filepath = '/data/users/jmahapatra/data/wordpairs_with_ser.txt'):
+def add_ser(wordpairs_filepath = '/data/users/jmahapatra/data/wordpairs.txt', word_phoneme_dict_filepath = '/data/users/jmahapatra/data/word_phoneme_dict.npy',wordpairs_ser_filepath = '/data/users/jmahapatra/data/wordpairs_with_ser.txt'):
 
 	#Read the wordpairs file
 	wordpairs_df = pd.read_csv(wordpairs_filepath)
 
+
+	#Read the Word phoneme dict
+	word_phoneme_dict = np.load(word_phoneme_dict_filepath, allow_pickle = True)
+
+
 	print('Calculating phonetic ser')
 
 	#Add the phonetic ser w.r.t word_1 and word_2 respectively
-	wordpairs_df["word_1_phonetic_ser"] = wordpairs_df.apply(lambda row: row["phonetic_edit_distance"]/len(row["word_1"]), axis = 1)
-	wordpairs_df["word_2_phonetic_ser"] = wordpairs_df.apply(lambda row: row["phonetic_edit_distance"]/len(row["word_2"]), axis = 1)
+	wordpairs_df["word_1_phonetic_ser"] = wordpairs_df.apply(lambda row: row["phonetic_edit_distance"]/len(word_phoneme_dict.item().get(row["word_1"]).split()), axis = 1)
+	wordpairs_df["word_2_phonetic_ser"] = wordpairs_df.apply(lambda row: row["phonetic_edit_distance"]/len(word_phoneme_dict.item().get(row["word_2"]).split()), axis = 1)
 
 	print('Calculating orthographic ser')
 
@@ -33,13 +38,14 @@ def add_ser(wordpairs_filepath = '/data/users/jmahapatra/data/wordpairs.txt', wo
 if __name__ == '__main__':
 
 	#wordpairs_filepath = 'Data/wordpairs_test.txt'
-	#wordpairs_ser_filepath = 'Data/wordpairs_test_with_ser.txt'
-	#add_ser(wordpairs_filepath, wordpairs_ser_filepath)
+	#word_phoneme_dict_filepath = 'Data/word_phoneme_dict.npy'
+	#wordpairs_ser_filepath = 'Data/bla_wordpairs_test_with_ser.txt'
 
 	wordpairs_filepath = '/data/users/jmahapatra/data/wordpairs.txt'
+	word_phoneme_dict_filepath = '/data/users/jmahapatra/data/word_phoneme_dict.npy'
 	wordpairs_ser_filepath = '/data/users/jmahapatra/data/wordpairs_with_ser.txt'
 	
-	add_ser(wordpairs_filepath, wordpairs_ser_filepath)
+	add_ser(wordpairs_filepath, word_phoneme_dict_filepath, wordpairs_ser_filepath)
 
 
 	
--- generate_dataframes.py
+++ generate_dataframes.py
@@ -26,12 +26,13 @@ if __name__ == '__main__':
 	wordpairs_filepath = 'Data/wordpairs_test.txt'
 	wordpairs_ser_filepath = 'Data/wordpairs_test_with_ser.txt'
 	word_embedding_dict_filepath = 'Data/word_embedding_dict.npy'
+	word_phoneme_dict_filepath = 'Data/word_phoneme_dict.npy'
 	#homophones_filepath = 'Data/homophones.txt'
 	#sim_nearest_neighbours_filepath = 'Data/sim_nearest_neighbours.txt'
 	#edit_nearest_neighbours_filepath = 'Data/edit_nearest_neighbours.txt'
 
 	#generate_pairs(phoney, data_filepath,wordpairs_filepath)
-	add_ser(wordpairs_filepath, wordpairs_ser_filepath)
+	add_ser(wordpairs_filepath, word_phoneme_dict_filepath, wordpairs_ser_filepath)
 	#generate_homophones(wordpairs_filepath,homophones_filepath)
 	#generate_nn(phoney, wordpairs_filepath ,sim_nearest_neighbours_filepath, edit_nearest_neighbours_filepath)
 
