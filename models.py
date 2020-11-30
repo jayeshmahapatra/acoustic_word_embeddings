50374e5132b6f70430bd0fc4a233f0524ff984a2
Jayesh Mahapatra <jayeshmahapatra@gmail.com>
2020-11-30 16:13:00 +0100

    added dropout noisy model

--- models.py
+++ models.py
@@ -45,6 +45,50 @@ class SimpleNet(nn.Module):
         x = F.relu(self.fc2(x))
         return x.cpu().detach().numpy() if dev.type == 'cuda' else x.detach().numpy()
 
+class SimpleNet_with_dropout(nn.Module):
+    def __init__(self,num_output,p):
+        super(SimpleNet, self).__init__()
+        self.conv1 = nn.Conv1d(40,96,(10))
+        self.pool1 = nn.MaxPool1d(3)
+        self.conv2 = nn.Conv1d(96, 96, (8))
+        self.pool2 = nn.MaxPool1d(3)
+        #self.fc1 = nn.Linear(1728, 1024)
+        self.fc1 = nn.Linear(672, 1024)
+        self.fc2 = nn.Linear(1024, num_output)
+        self.sm = nn.Softmax(dim = 1)
+        self.dropout = nn.Dropout(p=p)
+    def forward(self, x):
+        x = self.pool1(F.relu(self.conv1(x)))
+        #print(x.shape)
+        x = self.pool2(F.relu(self.conv2(x)))
+        #print('Pre')
+        #print(x.shape)  
+        x = x.view(x.shape[0], -1)
+        #print('Post')
+        #print(x.shape)
+        x = self.dropout(F.relu(self.fc1(x)))
+        #print(x.shape)
+        x = self.dropout(F.relu(self.fc2(x)))
+        #print(x.shape)
+        x = F.log_softmax(x,dim=1)
+        #print(x.shape)
+        #print("Done")
+        return x
+    
+    def give_embeddings(self,x,dev):
+        x = self.pool1(F.relu(self.conv1(x)))
+        #print(x.shape)
+        x = self.pool2(F.relu(self.conv2(x)))
+        #print('Pre')
+        #print(x.shape)  
+        x = x.view(x.shape[0], -1)
+        #print('Post')
+        #print(x.shape)
+        x = F.relu(self.fc1(x))
+        #print(x.shape)
+        x = F.relu(self.fc2(x))
+        return x.cpu().detach().numpy() if dev.type == 'cuda' else x.detach().numpy()
+
 
 class SiameseNet(nn.Module):
     def __init__(self, dim_out = 1024):
