6ee4b8de2a03d6674f0f84e5ad397272e5c733a3
Jayesh Mahapatra <jayeshmahapatra@gmail.com>
2021-03-26 15:00:34 +0100

    mapping

--- evaluate_mixed_cnn.py
+++ evaluate_mixed_cnn.py
@@ -89,6 +89,8 @@ if __name__ == '__main__':
 	noisy_test_ds = CNN_dataset(split_set = "test", char_threshold = 5, frequency_bounds = (0,np.Inf), snr = snr, k = np.Inf, cluster = True)
 	clean_num_to_word, clean_word_to_num = clean_test_ds.num_to_word.copy(),clean_test_ds.word_to_num.copy()
 	del clean_test_ds
+	noisy_num_to_word, noisy_word_to_num = noisy_train_ds.num_to_word.copy(),noisy_train_ds.word_to_num.copy()
+
 
 
 	#Transform Labels of Noisy
